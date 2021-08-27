8e2b181e32f1eb31244fa117402749de5ffa178f
johntsangcn <1368362869@qq.com>
2021-08-27 23:24:10 +0800

    whiles

--- 4-4-e7.cpp
+++ 4-4-e7.cpp
@@ -1,30 +1,47 @@
+/*
+Author: John Tsang
+Date: 2021-08-27
+Remark:输入两个正整数x0, y0, 找出满足以下条件的p,q的个数
+       a) p,q是正整数
+       b) p,q以x0为最大公约数，以y0为最小公倍数
+*/
 #include<iostream>
 using namespace std;
 int main()
 {
-    /*
-    int x0,y0,maxn,smallest,sum,i,j,k;
-    x0 = y0 = maxn = sum = i = j = k = smallest = 0;
+    int x0,y0,biggest,smallest,sum,p,q,a,r,flag;
+    x0 = y0 = biggest = sum = smallest = p = q = a = r = flag = 0;
     cin >> x0 >> y0;
-    for(i = 1; i <= y0; i++)
+    if(x0 <=0 || y0 <=0 )
     {
-
-        for(j = 1,k = 1;j <= y0,k <= y0;j++,k++)
-            {
-
-            if(j % i == 0&&k % i == 0&&i > maxn)
-                maxn = i;
-            if(j * i == y0&&k * i == y0)
-                {
-                smallest = j * i;
-                smallest = k * i;
-                sum = sum + 1;
-                }
-            if(maxn == x0&&smallest == y0&&sum == 1)
-                cout << j << k;
-            }
-
+        cout << "positive number needed." << endl;
+        return -1;
     }
-    */
+    for(p = x0; p <= y0 && flag == 0; p += x0)
+    {
+        for(q = x0; q <= y0 && flag == 0; q += x0)
+        {
+            a = p;
+            biggest = q;
+            while(r = a % biggest)
+            {
+                a = biggest;
+                biggest = r;
+            }
+            smallest = p * q / biggest;
+            if(biggest == x0 && smallest == y0)
+            {
+                if (p < q)
+                {
+                    sum = sum + 2;
+                    //cout << p << " " << q << endl;
+                    //cout << q << " "  << p << endl;
+                }
+                else
+                    flag = 1;
+            }
+        }
+    }
+    cout << sum;
     return 0;
 }
