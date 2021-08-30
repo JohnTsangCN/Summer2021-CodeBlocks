5b460207b298f8bff5aef93207730ed6f03b9916
johntsangcn <1368362869@qq.com>
2021-08-30 21:58:54 +0800

    array

--- 5-2-e1.cpp
+++ 5-2-e1.cpp
@@ -7,24 +7,27 @@ Remark:输入n个数，将最小数放到数组的第一个位置，其余元素
 using namespace std;
 int main()
 {
-    int n,i;
+    int n,i,index = -1;
+    float a[200] = {},x;
     cin >> n;
-    float a[n + 1],smallest;
-    smallest = 0;
-    for(i = 1; i <= n; i++)
+    if(n <= 0)
+    {
+        cout << "False!";
+        return -10000;
+    }
+    for(i = 0; i <= n - 1; i++)
+    {
         cin >> a[i];
-    smallest = a[1];
-    for(i = 2; i <= n; i++)
-    {
-        if(a[i] < smallest)
-            smallest = a[i];
+        if(index == -1)
+            index = 0;
+        if(a[i] < a[index])
+            index = i;
     }
-    cout << smallest;
-    for(i = 1; i <= n; i++)
-    {
-        if(a[i] == smallest)
-            continue;
-        cout << " " << a[i];
-    }
+    x = a[index];
+    for(i = index; i >= 1; i--)
+        a[i] = a[i - 1];
+    a[0] = x;
+    //for(i = 0; i <= n - 1; i++)
+        //cout << a[i] << " ";
     return 0;
 }
